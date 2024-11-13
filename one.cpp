1dde6b7fd0af219157ac5ca6e12b76a101d5687d
jassisamuran <samuran3132@gmail.com>
2024-11-13 19:54:06 +0530

    code for minimum and maximum orders using and find average orders logic

--- one.cpp
+++ one.cpp
@@ -5,12 +5,14 @@ using namespace std;
 #include<vector>
 #include<map>
 #include<string>
+#include<iomanip>
+#include<limits>
 // struct for sales how data is added 
 struct SaleRecord{
     string date;
     string sku;
     double unitPrice;
-    double quantity;
+    int quantity;
     
 };
 
@@ -24,7 +26,7 @@ vector<SaleRecord> parseData(const string& filepath){
     while(getline(file,line)){
         stringstream ss(line);
         string date,item,quantityStr,priceStr;
-        double quantity;
+        int quantity;
         double price;
         //getline is used to read value up to ','
         getline(ss,date,',');
@@ -34,7 +36,7 @@ vector<SaleRecord> parseData(const string& filepath){
         
         quantity=stoi(quantityStr);
         price=stod(priceStr);
-        data.push_back({date,item,quantity,price});
+        data.push_back({date,item,price,quantity});
         
 
     }
@@ -44,8 +46,8 @@ void calculatesSales(const vector<SaleRecord>& data){
     double totalSales=0;
     map<string,double>monthlySales;//used string for storing month as string 
     map<string,map<string, int>> monthlyPopularItems;// used map with string for months popularitems 
-    map<string,map<string, int>>monthlyRevenueItems;
-
+    map<string,map<string, double>>monthlyRevenueItems;
+    map<string,map<string, vector<int>>> orderStats;
 
     for(const auto& record:data){
         string month=record.date.substr(0,7);
@@ -60,6 +62,9 @@ void calculatesSales(const vector<SaleRecord>& data){
 
         // Track revenue for each item
         monthlyRevenueItems[month][record.sku] += saleAmount;
+
+        // Track order quantities for stats
+        orderStats[month][record.sku].push_back(record.quantity);
 
     }
     cout<<"Total Sales of Store: "<<totalSales<<endl;
@@ -83,6 +88,7 @@ void calculatesSales(const vector<SaleRecord>& data){
         }
         cout<<items.first<<": Item: "<<popurItem<<" Quantity Sold: "<<maxQnty<<endl;
     }
+
     cout<<"Items Generating Most Revenue Each Month"<<endl;
     for(const auto&month_Items:monthlyRevenueItems){
         string revenueItem;
@@ -96,18 +102,50 @@ void calculatesSales(const vector<SaleRecord>& data){
         cout<<month_Items.first<<": Item: "<<revenueItem<<" Revenue: "<<maxRevenue<<endl;
     }
 
+    cout<<"Order statistics for Most popular Item Each Month"<<endl;
+
+    for(const auto& month_item:orderStats){
+        // Find the most popular item for this month
+        string popularItem;
+        int maxQuantity=0;
+        for(const auto & item_quantities:monthlyPopularItems[month_item.first]){
+            if(item_quantities.second>maxQuantity){
+                maxQuantity=item_quantities.second;
+                popularItem=item_quantities.first;
+            }
+        }
+        
+        // Get the order quantities for the most popular item
+        const auto &quantities=orderStats[month_item.first][popularItem];
+        int minOrders=numeric_limits<int>::max();
+        int maxOrders=0;
+        int totalOrders=0;
+        
+        for(int qty:quantities){
+            if(qty<minOrders)minOrders=qty;
+            if(qty>maxOrders)maxOrders=qty;
+            totalOrders+=qty;
+        }
+        double avgOrders=quantities.empty()?0:static_cast<double>(totalOrders)/quantities.size();
+        cout<<month_item.first<<" "<<popularItem<<
+            ", Min Orders: "<<minOrders<<
+            ", Max Orders: "<<maxOrders<<
+            ", Average Orders: "<<fixed<< setprecision(2)<<avgOrders<<endl;
+    }
+
 }
+
 int main(){
-    std::ifstream file("one.txt");
-    if(!file.is_open()){
-        // if there is error opening in file then then close file()
-        cout<<"error opening of file";
-        return 0;
-    }
-    else
-    {
-        // cout<<"file is opened"<<endl;
-    }
+    // std::ifstream file("one.txt");
+    // if(!file.is_open()){
+    //     // if there is error opening in file then then close file()
+    //     cout<<"error opening of file";
+    //     return 0;
+    // }
+    // else
+    // {
+    //     // cout<<"file is opened"<<endl;
+    // }
     
     vector<SaleRecord> data=parseData("one.txt");
     calculatesSales(data);
